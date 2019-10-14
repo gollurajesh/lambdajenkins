70d03dfc0388b086cd099fab871dd2ea6b418d68
gollurajesh <gollurajesh23@gmail.com>
2019-10-14 16:44:59 +0530

    Update LambdaFunctionHandlerTest.java
    c

--- src/test/java/com/amazonaws/lambda/demo/LambdaFunctionHandlerTest.java
+++ src/test/java/com/amazonaws/lambda/demo/LambdaFunctionHandlerTest.java
@@ -38,6 +38,6 @@ public class LambdaFunctionHandlerTest {
         String output = handler.handleRequest(input, ctx);
 
         // TODO: validate output here if needed.
-        Assert.assertEquals("Hello from Lambda1!", output);
+        Assert.assertEquals("Hello from Lambda2!", output);
     }
 }
